dff6546955bb878dbca3ecacc4067c53ca3bc1a5
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2024-05-30 23:15:31 +0200

    Fix base type for Ray_ElectoonTeethComponent_Template so the actor serializes correctly

--- Assets/Scripts/UbiArt/DataTypes/Classes/Autogenerated/Main/ITF/Ray_ElectoonTeethComponent_Template.cs
+++ Assets/Scripts/UbiArt/DataTypes/Classes/Autogenerated/Main/ITF/Ray_ElectoonTeethComponent_Template.cs
@@ -1,6 +1,6 @@
 namespace UbiArt.ITF {
 	[Games(GameFlags.RO)]
-	public partial class Ray_ElectoonTeethComponent_Template : CSerializable {
+	public partial class Ray_ElectoonTeethComponent_Template : ActorComponent_Template {
 		public float xFromRight;
 		public float yFromTop;
 		public float itemWidth;
